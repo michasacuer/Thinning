d22c199e6d114e229c63107e51f826abc0c3066f
michasacuer <michasacuer3@gmail.com>
2019-11-02 08:06:28 +0100

    Application setup fix

--- WPF/Thinning.Contracts/ApplicationSetup.cs
+++ WPF/Thinning.Contracts/ApplicationSetup.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using Thinning.Algorithm;
     using Thinning.Infrastructure.Interfaces;
 
@@ -10,30 +11,29 @@
     {
         public List<string> GetRegisteredAlgorithmNames()
         {
-            var interfaceType = typeof(IAlgorithm);
-            var interfaces = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(interfaceType.IsAssignableFrom).ToList();
-
-            interfaces.Remove(typeof(IAlgorithm));
-
             var interfacesNames = new List<string>();
-            interfaces.ForEach(i => interfacesNames.Add(i.Name));
+            var assemblies = this.GetAlgorithmsAssemblies();
+            assemblies.ForEach(a => interfacesNames.Add(a.Name));
 
             return interfacesNames;
         }
 
         public List<IAlgorithm> GetRegisteredAlgorithmInstances()
         {
+            var assemblies = this.GetAlgorithmsAssemblies();
+            var algorithmInstances = new List<IAlgorithm>();
+            assemblies.ForEach(algorithm => algorithmInstances.Add((IAlgorithm)Activator.CreateInstance(algorithm)));
+
+            return algorithmInstances;
+        }
+
+        private List<TypeInfo> GetAlgorithmsAssemblies()
+        {
             var algorithmAssemly = typeof(K3M).Assembly;
             var algorithmAssemblies = algorithmAssemly.DefinedTypes.Where(type =>
                     type.ImplementedInterfaces.Any(inter => inter == typeof(IAlgorithm))).ToList();
 
-            var algorithmInstances = new List<IAlgorithm>();
-            algorithmAssemblies.ForEach(algorithm =>
-                algorithmInstances.Add((IAlgorithm)Activator.CreateInstance(algorithm)));
-
-            return algorithmInstances;
+            return algorithmAssemblies;
         }
     }
 }
